8bc00bcc1ed4bbc6aef633080659e8d7644d06cd
Udo Urbantschitsch <udo.urbantschitsch@cloudwerkstatt.com>
2014-05-25 20:15:20 +0200

    Twitter Bootstrap
    Basic implementation

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'devise', '3.2.4'
 gem 'paper_trail', '~> 3.0.2'
 gem 'acts-as-taggable-on'
 gem "ransack"
+gem 'bootstrap-sass', '2.3.2.0'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :contacts
 
   devise_for :users
+
+  get 'static_pages/contact'
+  get 'about' => 'static_pages#about'
+  get 'contactus' => 'static_pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
